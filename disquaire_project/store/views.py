26ef740ad9fbc826668922a162a754329477b3dc
mtresalmauroz <mtresalmauroz@gmail.com>
2017-12-17 15:28:28 +0100

    Route detail is now working

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import ALBUMS
+from .models import ALBUMS, ARTISTS
 
 # Create your views here.
 def index(request):
@@ -13,6 +13,17 @@ def store(request):
 	return HttpResponse(message)
 
 def listing(request):
+	"""Affiche le listing des albums en base"""
 	albums = ['<li>{}</li>'.format(album['name']) for album in ALBUMS]
 	message = """<ul>{}</ul>""".format('\n'.join(albums))
 	return HttpResponse(message)
+
+def detail(request, album_id):
+	id = int(album_id)
+	album = ALBUMS[id]
+	artists = " ".join([artist['name'] for artist in album['artists']])
+	message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+	return HttpResponse(message)
+
+
+
