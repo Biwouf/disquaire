f9980e8350048140bb8964e43583649af6caf22d
mtresalmauroz <mtresalmauroz@gmail.com>
2017-12-17 15:05:58 +0100

    Affichage de la liste des albums

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import ALBUMS
+
 # Create your views here.
 def index(request):
 	message = 'Salut tout le monde'
@@ -8,4 +10,9 @@ def index(request):
 
 def store(request):
 	message = 'Bienvenue sur la page store'
+	return HttpResponse(message)
+
+def listing(request):
+	albums = ['<li>{}</li>'.format(album['name']) for album in ALBUMS]
+	message = """<ul>{}</ul>""".format('\n'.join(albums))
 	return HttpResponse(message)
