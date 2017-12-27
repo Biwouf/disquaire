b021bf327effe23f235ea9cea1a9279876a1c1fb
mtresalmauroz <mtresalmauroz@gmail.com>
2017-12-27 15:42:02 +0100

    Detail view rewrite

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -5,7 +5,10 @@ from .models import Album, Artist, Contact, Booking
 
 # Create your views here.
 def index(request):
+	"""Page d'accueil"""
+	#On trie par date de création. On affiche que les trois derniers albums créés)
 	albums = Album.objects.filter(available=True).order_by('-created_at')[:3]
+	complete_message = (album.title for album in albums)
 	message = """
 	Voici la liste des derniers albums disponibles : 
 	<ul>
@@ -13,7 +16,7 @@ def index(request):
 		{}
 		</li>
 	</ul>
-	""".format("</li><li>".join(album.title for album in albums))
+	""".format("</li><li>".join(complete_message))
 	return HttpResponse(message)
 
 def store(request):
@@ -22,16 +25,15 @@ def store(request):
 
 def listing(request):
 	"""Affiche le listing des albums en base"""
-	albums = ['<li>{}</li>'.format(album['name']) for album in ALBUMS]
-	message = """<ul>{}</ul>""".format('\n'.join(albums))
+	albums = Album.objects.filter(available=True).order_by('-created_at')
+	message = """<ul><li>{}</li></ul>""".format('</li><li>'.join(album.title for album in albums))
 	return HttpResponse(message)
 
 def detail(request, album_id):
 	"""Affiche le détail d'un album. Url attendue : store/{id}"""
-	id = int(album_id)
-	album = ALBUMS[id]
-	artists = " ".join([artist['name'] for artist in album['artists']])
-	message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+	album = Album.objects.get(pk=album_id)
+	artists = " ".join(artist.name for artist in album.artists.all())
+	message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
 	return HttpResponse(message)
 
 def search(request):
